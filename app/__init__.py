c2ddf0835ff9a7bac91d78440e8fdd18f3a03483
raghuramjee7 <raghuramjee@outlook.com>
2022-10-18 03:12:02 +0530

    database

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, current_app
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
 app = Flask(__name__)
+
+ 
+
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
+
 
 from app import routes, models
--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from app import db
 
-class LoginTable(db.Model):
+class Login(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -8,3 +8,15 @@ class LoginTable(db.Model):
 
     def __repr__(self):
         return "<User {}>".format(self.username)
+    
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    user_id = db.Column(db.Integer, db.ForeignKey("login.id"))
+    github = db.Column(db.String(128))
+    skills = db.Column(db.String(1000))
+    interests = db.Column(db.String(1000))
+
+    def __repr__(self):
+        return "Commited"
